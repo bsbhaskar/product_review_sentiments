3fc103a3b74101a0b85968f15d9176b956e6289f
bsbhaskar <bsbhaskar@gmail.com>
2018-05-08 11:41:02 -0700

    initial attempt at clustering topics

--- scripts/review.sql
+++ scripts/review.sql
@@ -1,1 +1,1 @@
-CREATE TABLE reviews (id SERIAL PRIMARY KEY, product_name varchar (300), url varchar (300), page_no integer, r_no integer, r_stars varchar(30), r_date varchar(30), r_title varchar (300), r_text text, r_comments varchar (300) );
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, product_name varchar (300), url varchar (300), page_no integer, r_no integer, r_stars varchar(30), r_date varchar(30), r_name varchar(100), r_title varchar (300), r_text text, r_comments varchar (300) );
